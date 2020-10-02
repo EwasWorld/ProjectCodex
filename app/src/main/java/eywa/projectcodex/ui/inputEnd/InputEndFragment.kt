ad557507e3b5f724acfc00a399ed83c9535c798e
Ewa <ella.caldeirahankey@ntlworld.com>
2020-10-02 12:26:01 +0100

    Remove onActivityCreated

--- app/src/main/java/eywa/projectcodex/ui/inputEnd/InputEndFragment.kt
+++ app/src/main/java/eywa/projectcodex/ui/inputEnd/InputEndFragment.kt
@@ -33,12 +33,7 @@ class InputEndFragment : Fragment() {
     private var distanceUnit: String = ""
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_input_end, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        activity?.title = getString(R.string.input_end__title)
+        val view = inflater.inflate(R.layout.fragment_input_end, container, false)
 
         inputEndViewModel = ViewModelProvider(this, ViewModelFactory {
             InputEndViewModel(requireActivity().application, args.archerRoundId)
@@ -47,30 +42,33 @@ class InputEndFragment : Fragment() {
             archerRound.roundId?.let { roundId ->
                 inputEndViewModel.getArrowCountsForRound(roundId).observe(viewLifecycleOwner, Observer {
                     arrowCounts = it
-                    updateRoundInfo()
+                    updateRoundInfo(view)
                 })
                 inputEndViewModel.getDistancesForRound(roundId, archerRound.roundSubTypeId)
                         .observe(viewLifecycleOwner, Observer {
                             distances = it
-                            updateRoundInfo()
+                            updateRoundInfo(view)
                         })
                 inputEndViewModel.getRoundById(roundId).observe(viewLifecycleOwner, Observer {
                     distanceUnit =
                             getString(if (it.isMetric) R.string.units_meters_short else R.string.units_yards_short)
-                    updateRoundInfo()
+                    updateRoundInfo(view)
                 })
             }
         })
         inputEndViewModel.arrows.observe(viewLifecycleOwner, Observer { arrows ->
             arrows?.let {
                 this.arrows = arrows
-                updateRoundInfo()
+                updateRoundInfo(view)
             }
         })
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        activity?.title = getString(R.string.input_end__title)
         val endInputsFragment =
                 childFragmentManager.findFragmentById(R.id.fragment_input_end__end_inputs)!! as EndInputsFragment
 
@@ -115,45 +113,43 @@ class InputEndFragment : Fragment() {
      * Updates round indicators (72 arrows left at 100yd, etc.)
      * Updates the scores indicator table (current score, arrow count, etc.)
      */
-    private fun updateRoundInfo() {
-        view?.let { view ->
-            /*
-             * Scores indicator table
-             */
-            val scoreIndicatorFragment =
-                    childFragmentManager.findFragmentById(R.id.fragment_input_end__score_indicator)!!
-                            as ScoreIndicatorFragment
-            scoreIndicatorFragment.update(arrows)
+    private fun updateRoundInfo(view: View) {
+        /*
+         * Scores indicator table
+         */
+        val scoreIndicatorFragment =
+                childFragmentManager.findFragmentById(R.id.fragment_input_end__score_indicator)!!
+                        as ScoreIndicatorFragment
+        scoreIndicatorFragment.update(arrows)
 
-            /*
-             * Round Indicators
-             */
-            val roundIndicatorSection = view.findViewById<LinearLayout>(R.id.layout_input_end__remaining_arrows)
-            if (!args.showRemaining || distances.size != arrowCounts.size || distanceUnit.isBlank()) {
-                roundIndicatorSection.visibility = View.GONE
-                return
-            }
+        /*
+         * Round Indicators
+         */
+        val roundIndicatorSection = view.findViewById<LinearLayout>(R.id.layout_input_end__remaining_arrows)
+        if (!args.showRemaining || distances.size != arrowCounts.size || distanceUnit.isBlank()) {
+            roundIndicatorSection.visibility = View.GONE
+            return
+        }
 
-            roundIndicatorSection.visibility = View.VISIBLE
-            val roundIndicators = getRemainingArrowsPerDistance(
-                    arrows.size, arrowCounts, distances, distanceUnit,
-                    view.resources.getString(R.string.input_end__round_indicator_at)
-            )
-            val label = view.findViewById<TextView>(R.id.text_input_end__remaining_arrows_label)
-            val large = view.findViewById<TextView>(R.id.text_input_end__remaining_arrows_current_distance)
-            val small = view.findViewById<TextView>(R.id.text_input_end__remaining_arrows_later_distances)
-            if (roundIndicators.first.isNotBlank()) {
-                label.text = view.resources.getString(R.string.input_end__round_indicator_label)
-                large.text = roundIndicators.first
-                small.text = roundIndicators.second
-                large.visibility = View.VISIBLE
-                small.visibility = View.VISIBLE
-            }
-            else {
-                label.text = view.resources.getString(R.string.input_end__round_indicator_complete)
-                large.visibility = View.GONE
-                small.visibility = View.GONE
-            }
+        roundIndicatorSection.visibility = View.VISIBLE
+        val roundIndicators = getRemainingArrowsPerDistance(
+                arrows.size, arrowCounts, distances, distanceUnit,
+                view.resources.getString(R.string.input_end__round_indicator_at)
+        )
+        val label = view.findViewById<TextView>(R.id.text_input_end__remaining_arrows_label)
+        val large = view.findViewById<TextView>(R.id.text_input_end__remaining_arrows_current_distance)
+        val small = view.findViewById<TextView>(R.id.text_input_end__remaining_arrows_later_distances)
+        if (roundIndicators.first.isNotBlank()) {
+            label.text = view.resources.getString(R.string.input_end__round_indicator_label)
+            large.text = roundIndicators.first
+            small.text = roundIndicators.second
+            large.visibility = View.VISIBLE
+            small.visibility = View.VISIBLE
+        }
+        else {
+            label.text = view.resources.getString(R.string.input_end__round_indicator_complete)
+            large.visibility = View.GONE
+            small.visibility = View.GONE
         }
     }
 }
