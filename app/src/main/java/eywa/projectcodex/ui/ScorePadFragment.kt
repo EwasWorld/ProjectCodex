4faeb8046c041c37c09bf176a4cbb211e1e0c973
Ewa <ella.caldeirahankey@ntlworld.com>
2020-10-08 20:56:41 +0100

    Fixed ScorePad table rendering issue

--- app/src/main/java/eywa/projectcodex/ui/ScorePadFragment.kt
+++ app/src/main/java/eywa/projectcodex/ui/ScorePadFragment.kt
@@ -27,12 +27,56 @@ import kotlin.math.ceil
 class ScorePadFragment : Fragment() {
     private val args: ScorePadFragmentArgs by navArgs()
     private lateinit var scorePadViewModel: ScorePadViewModel
-    private var goldsType = GoldsType.TENS
-    private var arrowCounts = listOf<RoundArrowCount>()
-    private var distances = listOf<RoundDistance>()
-    private var distanceUnit: String? = null
-    private var arrows = listOf<ArrowValue>()
     private lateinit var tableAdapter: InfoTableViewAdapter
+
+    /**
+     * Used to prevent the table from messing itself up from too many refreshes
+     */
+    private var hasTableDataChanged = false
+
+    // TODO Is there a way to make these setters common?
+    private var goldsType = GoldsType.TENS
+        set(value) {
+            if (value == field) return
+            hasTableDataChanged = true
+            field = value
+            updateTable()
+        }
+    private var arrowCounts = listOf<RoundArrowCount>()
+        set(value) {
+            if (value.isNullOrEmpty() || value == field) return
+            if (!field.isNullOrEmpty()) hasTableDataChanged = true
+            field = value
+            updateTable()
+        }
+    private var distances = listOf<RoundDistance>()
+        set(value) {
+            if (value.isNullOrEmpty() || value == field) return
+            if (!field.isNullOrEmpty()) hasTableDataChanged = true
+            field = value
+            updateTable()
+        }
+    private var distanceUnit: String? = null
+        set(value) {
+            if (value.isNullOrEmpty() || value == field) return
+            if (!field.isNullOrEmpty()) hasTableDataChanged = true
+            field = value
+            updateTable()
+        }
+    private var arrows = listOf<ArrowValue>()
+        set(value) {
+            if (value.isNullOrEmpty() || value == field) return
+            if (!field.isNullOrEmpty()) hasTableDataChanged = true
+            field = value
+            updateTable()
+        }
+    private var endSize: Int? = null
+        set(value) {
+            if (value == null || value < 1 || value == field) return
+            hasTableDataChanged = true
+            field = value
+            updateTable()
+        }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_score_pad, container, false)
@@ -43,6 +87,8 @@ class ScorePadFragment : Fragment() {
         activity?.title = getString(R.string.score_pad__title)
 
         tableAdapter = InfoTableViewAdapter(context!!)
+        if (endSize == null) endSize = args.endSize
+        hasTableDataChanged = true
         val tableView = view.findViewById<TableView>(R.id.table_view_score_pad)
         tableView.adapter = tableAdapter
         tableView.tableViewListener = ScorePadTableViewListener(tableView)
@@ -57,12 +103,10 @@ class ScorePadFragment : Fragment() {
             archerRound.roundId?.let { roundId ->
                 scorePadViewModel.getArrowCountsForRound(roundId).observe(viewLifecycleOwner, Observer {
                     arrowCounts = it
-                    updateTable()
                 })
                 scorePadViewModel.getDistancesForRound(roundId, archerRound.roundSubTypeId)
                         .observe(viewLifecycleOwner, Observer {
                             distances = it
-                            updateTable()
                         })
             }
         })
@@ -70,16 +114,15 @@ class ScorePadFragment : Fragment() {
         // Get golds type and distance unit
         scorePadViewModel.roundInfo.observe(viewLifecycleOwner, Observer { round ->
             if (round == null) return@Observer
+            distanceUnit =
+                    getString(if (round.isMetric) R.string.units_meters_short else R.string.units_yards_short)
             goldsType = getGoldsType(round.isOutdoor, round.isMetric)
-            distanceUnit = getString(if (round.isMetric) R.string.units_meters_short else R.string.units_yards_short)
-            updateTable()
         })
 
         // Get arrows
         scorePadViewModel.arrowsForRound.observe(viewLifecycleOwner, Observer { arrowValues ->
             if (arrowValues == null) return@Observer
             arrows = arrowValues
-            updateTable()
         })
     }
 
@@ -87,21 +130,21 @@ class ScorePadFragment : Fragment() {
      * Does nothing if the requirements to make a valid table are not met
      */
     private fun updateTable() {
-        if (arrows.isNullOrEmpty() || arrowCounts.size != distances.size
-            || (arrowCounts.isNotEmpty() && distanceUnit == null)) {
+        if (endSize == null || arrows.isNullOrEmpty() || arrowCounts.size != distances.size ||
+            (distances.isNotEmpty() && distanceUnit.isNullOrBlank()) || !hasTableDataChanged) {
             return
         }
 
         try {
             val tableData = calculateScorePadTableData(
-                    arrows, args.endSize, goldsType, resources, arrowCounts, distances, distanceUnit
+                    arrows, endSize!!, goldsType, resources, arrowCounts, distances, distanceUnit
             )
-            val arrowRowsShot = ceil(arrows.size / args.endSize.toDouble()).toInt()
+            val arrowRowsShot = ceil(arrows.size / endSize!!.toDouble()).toInt()
             tableAdapter.setAllItems(
                     getColumnHeadersForTable(scorePadColumnHeaderIds, resources, goldsType),
                     generateNumberedRowHeaders(
                             if (arrowCounts.isNotEmpty()) {
-                                arrowCounts.map { ceil(it.arrowCount / args.endSize.toDouble()).toInt() }
+                                arrowCounts.map { ceil(it.arrowCount / endSize!!.toDouble()).toInt() }
                             }
                             else {
                                 listOf(arrowRowsShot)
@@ -112,6 +155,7 @@ class ScorePadFragment : Fragment() {
                     ),
                     tableData
             )
+            hasTableDataChanged = false
         }
         catch (e: IllegalArgumentException) {
             val builder = AlertDialog.Builder(activity)
