7476df0101373beae4af96d9707de15b3d6dda65
Ewa <ella.caldeirahankey@ntlworld.com>
2021-07-12 20:02:36 +0100

    Added handicap calculation logic

---
+++ app/src/main/java/eywa/projectcodex/components/archerRoundScore/Handicap.kt
@@ -0,0 +1,198 @@
+package eywa.projectcodex.components.archerRoundScore
+
+import eywa.projectcodex.database.rounds.Round
+import eywa.projectcodex.database.rounds.RoundArrowCount
+import eywa.projectcodex.database.rounds.RoundDistance
+import kotlin.math.exp
+import kotlin.math.floor
+import kotlin.math.pow
+import kotlin.math.roundToInt
+
+class Handicap {
+    companion object {
+        /**
+         * Binary search for the handicap of the given [score]. Will find the worst (highest) possible (e.g. 599 portsmouth
+         * can be 3, 4, or 5 so return 5)
+         *
+         * @param innerTenArcher whether or not the shooter uses inner ten scoring (unused if the round doesn't use inner
+         * ten scoring)
+         * @param arrows get the handicap only for the first this many arrows (if null then for a full round)
+         * @return handicap 0-100
+         */
+        fun getHandicapForRound(
+                round: Round,
+                roundArrowCounts: List<RoundArrowCount>,
+                roundDistances: List<RoundDistance>,
+                score: Int,
+                innerTenArcher: Boolean,
+                arrows: Int?
+        ): Int {
+            require(arrows == null || arrows > 0) { "Arrows must be greater than 0" }
+            require(roundArrowCounts.size == roundDistances.size) { "Arrow counts and distances size not equal" }
+            require(roundArrowCounts.all { it.roundId == round.roundId }) { "Arrow count round ID incorrect" }
+            require(roundDistances.all { it.roundId == round.roundId }) { "Distance round ID incorrect" }
+            require(roundDistances.distinctBy { it.subTypeId }.size == 1) { "Multiple subtypes given" }
+
+            var low = 0 // best possible handicap
+            var high = 100 // worst possible handicap
+            while (true) {
+                var testHC = floor((high + low) / 2.0).roundToInt()
+                val testHCScore =
+                        getScoreForRound(round, roundArrowCounts, roundDistances, testHC, innerTenArcher, arrows)
+                when {
+                    testHCScore == score -> {
+                        // Some scores can have multiple possible handicaps - find the worst (highest)
+                        while (
+                            getScoreForRound(
+                                    round, roundArrowCounts, roundDistances, testHC + 1, innerTenArcher, arrows
+                            ) == score
+                        ) {
+                            testHC++
+                        }
+                        return testHC
+                    }
+                    high - low == 1 -> return if (score < testHCScore) low else high
+                    score < testHCScore -> low = testHC
+                    else -> high = testHC
+                }
+            }
+        }
+
+        /**
+         * Calculates the expected score for [round] for an archer with [handicap]
+         *
+         * @param roundArrowCounts all arrow counts pertaining to [round], roundId field must be identical for all items
+         * @param roundDistances all distances pertaining to [round], roundId and sub type fields must be identical for all items
+         * @param innerTenArcher whether or not the shooter uses inner ten scoring (unused if the round doesn't use inner ten scoring)
+         * @param arrows null to calculate expected score for whole round, else calculate for X arrows shot
+         * @return expected score for the handicap
+         */
+        fun getScoreForRound(
+                round: Round,
+                roundArrowCounts: List<RoundArrowCount>,
+                roundDistances: List<RoundDistance>,
+                handicap: Int,
+                innerTenArcher: Boolean,
+                arrows: Int?
+        ): Int {
+            require(arrows == null || arrows > 0) { "Arrows must be greater than 0" }
+            require(roundArrowCounts.size == roundDistances.size) { "Arrow counts and distances size not equal" }
+            require(roundArrowCounts.all { it.roundId == round.roundId }) { "Arrow count round ID incorrect" }
+            require(roundDistances.all { it.roundId == round.roundId }) { "Distance round ID incorrect" }
+            require(roundDistances.distinctBy { it.subTypeId }.size == 1) { "Multiple subtypes given" }
+
+            // Get score
+            val scoringType = ScoringType.getScoringType(round)
+            var currentArrowCount = 0
+            var score = 0.0
+            for (countsDistances in roundArrowCounts.sortedBy { it.distanceNumber }
+                    .zip(roundDistances.sortedBy { it.distanceNumber })) {
+                var arrowCount = countsDistances.first.arrowCount
+                val faceSizeInCm = countsDistances.first.faceSizeInCm
+                val distance = countsDistances.second.distance
+                if (arrows == null || currentArrowCount < arrows) {
+                    if (arrows != null && currentArrowCount + arrowCount > arrows) {
+                        arrowCount = arrows - currentArrowCount
+                    }
+                    score += arrowCount * scoringType.averageScorePerArrow(
+                            if (round.isMetric) distance.toDouble() else distance * 0.9144,
+                            faceSizeInCm.roundToInt(),
+                            handicap.toDouble(),
+                            innerTenArcher && !round.isOutdoor
+                    )
+                    currentArrowCount += arrowCount
+                }
+            }
+            return score.roundToInt() // Truncate
+        }
+    }
+
+    /**
+     * How arrows on a face are scored e.g. 10-zone metric or 5-zone imperial
+     * Used for calculating average arrow score for a given handicap
+     */
+    enum class ScoringType(
+            // Constants used in averageScorePerArrow
+            private var initial: Int,
+            private var sumMultiplier: Int,
+            private var sumStart: Int,
+            private var sumEnd: Int,
+            private var sumInternalDenominator: Int,
+            private var subtractInternalDenominator: Double,
+            private var subtractMultiplier: Int
+    ) {
+        IMPERIAL(9, 2, 1, 4, 10, 2.0, 1),
+        METRIC(10, 1, 1, 10, 20, 0.0, 0),
+
+        // 40cm face cut off after the 6 ring (3 separate targets in a vertical line)
+        TRIPLE(10, 1, 1, 4, 20, 20.0 / 5.0, 6),
+        VEGAS(TRIPLE),
+
+        // 80cm face cut off after 6 ring
+        FITA_FIVE_ZONE(TRIPLE),
+        WORCESTER(5, 1, 1, 5, 10, 0.0, 0),
+
+        // 80cm face cut off after 5 ring
+        FITA_SIX_ZONE(10, 1, 1, 5, 20, 20.0 / 6.0, 5);
+
+        companion object {
+            // This is the diameter of an 1864
+            private const val arrowDiameterInCm = 0.357
+
+            // TODO Make a faces enum?
+            fun getScoringType(round: Round, face: String? = null): ScoringType {
+                return when {
+                    round.displayName.contains(WORCESTER.toString(), ignoreCase = true) -> WORCESTER
+                    round.displayName.contains(VEGAS.toString(), ignoreCase = true) -> VEGAS
+                    face != null && face.equals("triple", ignoreCase = true) -> TRIPLE
+                    face != null && face.equals("fita five", ignoreCase = true) -> FITA_FIVE_ZONE
+                    face != null && face.equals("fita six", ignoreCase = true) -> FITA_SIX_ZONE
+                    round.isMetric -> METRIC
+                    else -> IMPERIAL
+                }
+            }
+        }
+
+        constructor(scoringType: ScoringType) : this(
+                scoringType.initial,
+                scoringType.sumMultiplier,
+                scoringType.sumStart,
+                scoringType.sumEnd,
+                scoringType.sumInternalDenominator,
+                scoringType.subtractInternalDenominator,
+                scoringType.subtractMultiplier
+        )
+
+        /**
+         * Formulas found in The Construction of the Graduated Handicap Tables for Target Archery by David Lane
+         *
+         * @param innerTenScoring true if only the inner ten ring should be counted as 10
+         */
+        fun averageScorePerArrow(
+                rangeInM: Double,
+                faceSizeInCm: Int,
+                handicap: Double,
+                innerTenScoring: Boolean
+        ): Double {
+            val sigma = (rangeInM * 1.036.pow(handicap + 12.9)
+                    * 0.05
+                    * (1 + 0.000001429 * 1.07.pow(handicap + 4.3) * rangeInM.pow(2.0))
+                    ).pow(2.0)
+
+            fun expCalc(faceSizeInCm: Int, denominator: Double): Double =
+                    exp(-(faceSizeInCm.toDouble() / denominator + arrowDiameterInCm).pow(2.0) / sigma)
+
+            val sumStart = sumStart + if (innerTenScoring) 1 else 0
+            var total = 0.0
+            for (i in sumStart..sumEnd) {
+                total += expCalc(faceSizeInCm, sumInternalDenominator.toDouble() / i)
+            }
+            total = initial - sumMultiplier * total - (subtractMultiplier
+                    * expCalc(faceSizeInCm, subtractInternalDenominator))
+            if (innerTenScoring) {
+                total -= expCalc(faceSizeInCm, 40.0)
+            }
+            return total
+        }
+    }
+}
---
+++ app/src/test/java/eywa/projectcodex/HandicapUnitTest.kt
@@ -0,0 +1,216 @@
+package eywa.projectcodex
+
+import eywa.projectcodex.components.archerRoundScore.Handicap
+import eywa.projectcodex.database.rounds.Round
+import eywa.projectcodex.database.rounds.RoundArrowCount
+import eywa.projectcodex.database.rounds.RoundDistance
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * http://www.crystalpalacebowmen.org.uk/handicap.shtml appears to also implement the same formulas as me
+ * Codex's calculated numbers for the following rounds match this website but not printed handicap tables (circa 1998):
+ * - Long Metric V
+ */
+class HandicapUnitTest {
+    data class ScoreHandicapMapping(val score: Int, val handicap: Int)
+
+    class HandicapTableEntry(roundInfo: List<Any>, val mappings: List<ScoreHandicapMapping>, val useInnerTen: Boolean) {
+        val round: Round = roundInfo.filterIsInstance<Round>().first()
+        val arrowCounts: List<RoundArrowCount> = roundInfo.filterIsInstance<RoundArrowCount>()
+        val distances: List<RoundDistance> = roundInfo.filterIsInstance<RoundDistance>()
+    }
+
+    private val handicapTableEntries = listOf(
+            /*
+             * Imperial
+             */
+            HandicapTableEntry(
+                    listOf(
+                            Round(1, "western", "western", true, false, listOf()),
+                            RoundArrowCount(1, 1, 122.0, 48),
+                            RoundArrowCount(1, 2, 122.0, 48),
+                            RoundDistance(1, 1, 1, 60),
+                            RoundDistance(1, 2, 1, 50)
+                    ),
+                    listOf(
+                            // Sparse high score end
+                            ScoreHandicapMapping(864, 6),
+                            ScoreHandicapMapping(863, 9),
+                            ScoreHandicapMapping(862, 10),
+                            ScoreHandicapMapping(861, 12),
+                            ScoreHandicapMapping(859, 13),
+                            ScoreHandicapMapping(858, 14),
+                            // Mid range
+                            ScoreHandicapMapping(764, 35),
+                            ScoreHandicapMapping(756, 36),
+                            ScoreHandicapMapping(748, 37),
+                            ScoreHandicapMapping(739, 38),
+                            ScoreHandicapMapping(730, 39)
+                    ),
+                    true
+            ),
+            /*
+             * Metric
+             */
+            HandicapTableEntry(
+                    listOf(
+                            Round(2, "longmetricvi", "long metric vi", true, true, listOf()),
+                            RoundArrowCount(2, 1, 122.0, 36),
+                            RoundArrowCount(2, 2, 122.0, 36),
+                            RoundDistance(2, 1, 1, 40),
+                            RoundDistance(2, 2, 1, 30)
+                    ),
+                    listOf(
+                            // Sparse high score end
+                            ScoreHandicapMapping(719, 3),
+                            ScoreHandicapMapping(718, 5),
+                            ScoreHandicapMapping(717, 7),
+                            ScoreHandicapMapping(716, 8),
+                            ScoreHandicapMapping(715, 9),
+                            // Mid range
+                            ScoreHandicapMapping(340, 70),
+                            ScoreHandicapMapping(323, 71),
+                            ScoreHandicapMapping(306, 72),
+                            ScoreHandicapMapping(289, 73),
+                            ScoreHandicapMapping(272, 74)
+                    ),
+                    true
+            ),
+            HandicapTableEntry(
+                    listOf(
+                            Round(3, "longmetricgents", "long metric gents", true, true, listOf()),
+                            RoundArrowCount(3, 1, 122.0, 36),
+                            RoundArrowCount(3, 2, 122.0, 36),
+                            RoundDistance(3, 1, 1, 90),
+                            RoundDistance(3, 2, 1, 70)
+                    ),
+                    listOf(
+                            // Sparse low score end
+                            ScoreHandicapMapping(5, 83),
+                            ScoreHandicapMapping(4, 84),
+                            ScoreHandicapMapping(3, 86),
+                            ScoreHandicapMapping(2, 89),
+                            ScoreHandicapMapping(1, 95)
+                    ),
+                    true
+            ),
+            /*
+             * Vegas
+             */
+            // Recurve
+            HandicapTableEntry(
+                    listOf(
+                            Round(4, "vegas", "vegas", false, true, listOf("vegas")),
+                            RoundArrowCount(4, 1, 40.0, 60),
+                            RoundDistance(4, 1, 1, 18)
+                    ),
+                    listOf(
+                            ScoreHandicapMapping(294, 55),
+                            ScoreHandicapMapping(280, 56),
+                            ScoreHandicapMapping(267, 57),
+                            ScoreHandicapMapping(254, 58),
+                            ScoreHandicapMapping(241, 59)
+                    ),
+                    false
+            ),
+            // Compound
+            HandicapTableEntry(
+                    listOf(
+                            Round(4, "vegas", "vegas", false, true, listOf("vegas")),
+                            RoundArrowCount(4, 1, 40.0, 60),
+                            RoundDistance(4, 1, 1, 18)
+                    ),
+                    listOf(
+                            ScoreHandicapMapping(292, 55),
+                            ScoreHandicapMapping(278, 56),
+                            ScoreHandicapMapping(265, 57),
+                            ScoreHandicapMapping(252, 58),
+                            ScoreHandicapMapping(240, 59)
+                    ),
+                    true
+            ),
+            /*
+             * Worcester
+             */
+            HandicapTableEntry(
+                    listOf(
+                            Round(5, "worcester", "worcester", false, false, listOf(), fiveArrowEnd = true),
+                            RoundArrowCount(5, 1, 16 * 2.54, 60),
+                            RoundDistance(5, 1, 1, 20)
+                    ),
+                    listOf(
+                            ScoreHandicapMapping(241, 45),
+                            ScoreHandicapMapping(237, 46),
+                            ScoreHandicapMapping(234, 47),
+                            ScoreHandicapMapping(230, 48),
+                            ScoreHandicapMapping(226, 49)
+                    ),
+                    false
+            )
+    )
+
+    @Test
+    fun testHandicapCalculations() {
+        for (hcEntry in handicapTableEntries) {
+            var previous: ScoreHandicapMapping? = null
+            for (mapping in hcEntry.mappings) {
+                // If the handicaps differ by one but the scores differ by more than one, test all values in the score
+                //      range to ensure the worst handicap is always given
+                val range = when (mapping.handicap) {
+                    previous?.handicap?.plus(1) -> (previous.score + 1)..mapping.score
+                    else -> mapping.score.rangeTo(mapping.score)
+                }
+                for (score in range) {
+                    Assert.assertEquals(
+                            "Incorrect handicap for '${hcEntry.round.displayName}' score $score",
+                            mapping.handicap,
+                            Handicap.getHandicapForRound(
+                                    hcEntry.round,
+                                    hcEntry.arrowCounts,
+                                    hcEntry.distances,
+                                    score,
+                                    hcEntry.useInnerTen,
+                                    null
+                            )
+                    )
+                }
+                previous = mapping
+            }
+        }
+    }
+
+    @Test
+    fun testScoreCalculations() {
+        for (hcEntry in handicapTableEntries) {
+            var previous: ScoreHandicapMapping? = null
+            for (mapping in hcEntry.mappings) {
+                val delta = if (hcEntry.round.displayName.contains("worcester", ignoreCase = true)) 1.0 else 0.0
+
+                // If the scores differ by one but the handicaps differ by more than one, test all values in the
+                //      handicap to ensure the correct score is always given
+                val startRangeAt = when (mapping.score) {
+                    previous?.score?.plus(1) -> (previous.handicap + 1)
+                    else -> mapping.handicap
+                }
+
+                for (handicap in startRangeAt..mapping.handicap) {
+                    Assert.assertEquals(
+                            "Incorrect score for '${hcEntry.round.displayName}' handicap $handicap",
+                            mapping.score.toDouble(),
+                            Handicap.getScoreForRound(
+                                    hcEntry.round,
+                                    hcEntry.arrowCounts,
+                                    hcEntry.distances,
+                                    handicap,
+                                    hcEntry.useInnerTen,
+                                    null
+                            ).toDouble(),
+                            delta
+                    )
+                }
+                previous = mapping
+            }
+        }
+    }
+}
