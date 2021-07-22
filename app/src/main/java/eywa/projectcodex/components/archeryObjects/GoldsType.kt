1af6fa2a8723581130ae80d0a75114dfbf5446b1
Ewa <ella.caldeirahankey@ntlworld.com>
2021-07-22 10:20:48 +0100

    Change how golds are counted for outdoor metric rounds from Xs only to 10s

--- app/src/main/java/eywa/projectcodex/components/archeryObjects/GoldsType.kt
+++ app/src/main/java/eywa/projectcodex/components/archeryObjects/GoldsType.kt
@@ -4,24 +4,26 @@ import eywa.projectcodex.R
 import eywa.projectcodex.database.arrowValue.ArrowValue
 
 /**
- * @return which golds type should be used
+ * @return which golds type should be used based on whether the round [isOutdoor] and/or [isMetric]
  */
 fun getGoldsType(isOutdoor: Boolean, isMetric: Boolean): GoldsType {
     return when {
         !isOutdoor -> GoldsType.TENS
-        isMetric -> GoldsType.XS
+        isMetric -> GoldsType.TENS
         else -> GoldsType.NINES
     }
 }
 
 /**
- * The minimum arrow value to be counted as a gold
+ * Represents how golds will be calculated
+ *
+ * @param score the minimum score required for an arrow to be counted as a gold
+ * @param isX whether an X is required for the arrow to be counted as a gold
+ * @param colHeaderStringId the string to display in the score pad for the golds column
  */
 enum class GoldsType(private val score: Int, private val isX: Boolean, val colHeaderStringId: Int) {
-    NINES(9, false, R.string.table_golds_nines_header), TENS(
-            10, false,
-            R.string.table_golds_tens_header
-    ),
+    NINES(9, false, R.string.table_golds_nines_header),
+    TENS(10, false, R.string.table_golds_tens_header),
     XS(10, true, R.string.table_golds_xs_header);
 
     fun isGold(arrow: Arrow): Boolean {
