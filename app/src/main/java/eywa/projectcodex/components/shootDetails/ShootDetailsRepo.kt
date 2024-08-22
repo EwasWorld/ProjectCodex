7e69aeda033c9328bf2bb58501723f8dfa8b6c83
Ewa <ella.caldeirahankey@ntlworld.com>
2024-08-22 16:56:45 +0100

    Fix deleting the most recent round then creating a new round causing an error

--- app/src/main/java/eywa/projectcodex/components/shootDetails/ShootDetailsRepo.kt
+++ app/src/main/java/eywa/projectcodex/components/shootDetails/ShootDetailsRepo.kt
@@ -109,7 +109,6 @@ class ShootDetailsRepo(
         launch {
             state
                     .map { it.shootId }
-                    .distinctUntilChanged()
                     .flatMapLatest { shootId ->
                         if (shootId == null) {
                             return@flatMapLatest flow<Pair<Int?, DatabaseFullShootInfo?>> { emit(null to null) }
@@ -137,7 +136,6 @@ class ShootDetailsRepo(
         launch {
             state
                     .map { it.fullShootInfo?.shootRound?.let { sr -> sr.roundId to sr.roundSubTypeId } }
-                    .distinctUntilChanged()
                     .flatMapLatest {
                         if (it == null) {
                             return@flatMapLatest flow<Pair<DbShortShoots?, DbShortShoots?>> { emit(null to null) }
@@ -146,7 +144,7 @@ class ShootDetailsRepo(
                         db.shootsRepo()
                                 .getMostRecentShootsForRound(10, roundId, subTypeId ?: 1)
                                 .combine(
-                                        db.shootsRepo().getHighestScoreShootsForRound(10, roundId, subTypeId ?: 1)
+                                        db.shootsRepo().getHighestScoreShootsForRound(10, roundId, subTypeId ?: 1),
                                 ) { latest, pbs -> latest to pbs }
                     }.collect { (latest, pbs) ->
                         state.update { it.copy(roundPbs = pbs, pastRoundRecords = latest) }
@@ -156,9 +154,9 @@ class ShootDetailsRepo(
             state
                     .map {
                         val distance = it.fullShootInfo?.remainingArrowsAtDistances?.firstOrNull()?.second
+                                ?: it.fullShootInfo?.fullRoundInfo?.roundDistances?.maxOfOrNull { d -> d.distance }
                         distance to it.fullShootInfo?.round?.isMetric
                     }
-                    .distinctUntilChanged()
                     .flatMapLatest { (distance, isMetric) ->
                         if (distance == null || isMetric == null) return@flatMapLatest emptyFlow()
                         db.sightMarkRepo().getSightMarkForDistance(distance, isMetric)
@@ -189,11 +187,8 @@ class ShootDetailsRepo(
 
     private fun setupState(shootId: Int?) {
         when {
-            shootId == null ->
-                state.update { ShootDetailsState(isError = true).preserveFixedInfo(it) }
-
-            state.value.shootId != shootId ->
-                state.update { ShootDetailsState(shootId = shootId).preserveFixedInfo(it) }
+            shootId == null -> state.update { ShootDetailsState(isError = true).preserveFixedInfo(it) }
+            else -> state.update { ShootDetailsState(shootId = shootId).preserveFixedInfo(it) }
         }
     }
 
